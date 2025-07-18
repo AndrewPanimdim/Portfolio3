32f416fb0b63e7a24ef966efadaca75ee34b0c77
Andrew Panimdim <baelmeans74@gmail.com>
2025-07-18 16:00:23 +0800

    a

--- JavaScript.js
+++ JavaScript.js
@@ -14,20 +14,3 @@ window.addEventListener('scroll', function() {
     lastScrollY = window.scrollY;
 });
 
-
-
-function AnimationSlideInLeft(selector, animationClass = 'slideLeft', threshold = 0.5) {
-  const elements = document.querySelectorAll(selector);
-  const observer = new IntersectionObserver((entries, observerInstance) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add(animationClass);
-        observerInstance.unobserve(entry.target);
-      }
-    });
-  }, { threshold });
-  elements.forEach(el => observer.observe(el));
-}
-
-
-AnimationSlideInLeft('#project');
